aba43284f4b47566a3fc00f29457c0551c076ae4
vavraj <rajvaibhav43210@gmail.com>
2024-05-13 00:33:42 +0530

    changes in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,7 +19,7 @@ const PageAbout = ({}) => {
         <SectionHero
           heading="👋 About Us."
           btnText=""
-          subHeading="We’re impartialImmerse yourself in a world of refined sophistication and timeless allure, as we invite you to experience the essence of luxury encapsulated in every drop."
+          subHeading="We’re impartial Immerse yourself in a world of refined sophistication and timeless allure, as we invite you to experience the essence of luxury encapsulated in every drop."
         />
         </div>
         
